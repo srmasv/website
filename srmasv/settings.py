d62f87cf59ba937332fdd6ea25a0b14c594cfaa5
Akash Purandare <akash.p1997@gmail.com>
2018-10-19 07:42:13 +0530

    Updating the settings

--- srmasv/settings.py
+++ srmasv/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'srmasv.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'srmasv/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
