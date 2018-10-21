e4bc8c37f810c9da38b1c7cd1ff95460fdb0fd9b
Akash Purandare <akash.p1997@gmail.com>
2018-10-22 00:03:34 +0530

    Added cloudcube media files support

--- srmasv/settings.py
+++ srmasv/settings.py
@@ -127,12 +127,12 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
-STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'srmasv.storage_backends.MediaStorage'
+STATIC_URL = '%s/static/' % AWS_S3_CUSTOM_DOMAIN
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
