6f1f91cdbb1793549b31f7d69077e5288f2f9e81
Akash Purandare <akash.p1997@gmail.com>
2018-10-28 07:16:38 +0530

    Added carousel items support

--- navbar/models.py
+++ navbar/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.forms import ValidationError
 
+from django.conf import settings
+from PIL import Image
+import os, platform
+
 # Create your models here.
 class NavItem(models.Model):
     text = models.CharField(max_length=10)
@@ -31,16 +35,37 @@ class HomeText(models.Model):
             raise ValidationError("Only one instance of HomeText can be made.")
 
 class CarouselItem(models.Model):
-    title = models.CharField(max_length=40)
-    subtitle = models.CharField(max_length=255)
-    image = models.ImageField(upload_to="images/")
-
-    def __str__(self):
-        return self.title
+    title = models.CharField(max_length=40, blank=True)
+    subtitle = models.CharField(max_length=255, blank=True)
+    item = models.FileField(upload_to="carousel/", help_text="This image/video will always take some time to process for each save. Please be patient.")
+    is_photo = models.BooleanField(default=True)
+    extension = models.CharField(max_length=4, help_text="You need to assign the value yourself.")
 
     class Meta:
         verbose_name = "Carousel Item"
         verbose_name_plural = "Carousel Items"
+
+    def save(self, *args, **kwargs):
+        super(CarouselItem, self).save(*args, **kwargs)
+        filename = str(self.item.file)
+        try:
+            image = Image.open(filename, "r")
+            new_image = image.resize((800, 400))
+            new_image.save(filename)
+            self.is_photo = True
+
+        except OSError:
+            #Not an Image
+            if platform.system()=="Darwin":
+                ffmpeg = os.path.join(settings.BASE_DIR, "ffmpeg-osx")
+            elif platform.system()=="Linux":
+                ffmpeg = os.path.join(settings.BASE_DIR, "ffpmeg-linux")
+            else:
+                return
+            os.system(ffmpeg + ' -i %s -vf "scale=800:400:force_original_aspect_ratio=decrease,pad=800:400:(ow-iw)/2:(oh-ih)/2" %s1%s' % (filename, filename[:-4], filename[-4:]))
+            os.remove("%s" % filename)
+            os.rename("%s1%s" % (filename[:-4], filename[-4:]), "%s" % filename)
+            self.is_photo = False
 
 class JourneyText(models.Model):
     heading = models.CharField(max_length=20)
--- navbar/views.py
+++ navbar/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import HomeText, JourneyText, DomainText
+from .models import HomeText, JourneyText, DomainText, CarouselItem
 
 # Create your views here.
 from srmasv.views import get_all_navitems, get_breadcrumbs
@@ -14,11 +14,15 @@ def get_journeytext():
 def get_domains():
     return DomainText.objects.order_by("position")
 
+def get_carouselitems():
+    return CarouselItem.objects.order_by("-id")
+
 def home(request):
     params = {}
     params["navitems"] = get_all_navitems
     params["breadcrumbs"] = get_breadcrumbs(request)
     params["hometext"] = get_hometext()
+    params["carouselitems"] = get_carouselitems()
     params["journeytext"] = get_journeytext()
     params["domains"] = get_domains()
     return render(request, 'home.html', params)
