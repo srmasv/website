ef211ce7bb6adace57ddc15e02ad2d415ba2d2ae
Akash Purandare <akash.p1997@gmail.com>
2018-10-30 10:45:11 +0530

    Fixed the usage of subprocess for validation

--- navbar/models.py
+++ navbar/models.py
@@ -56,8 +56,8 @@ class CarouselItem(models.Model):
             new_image = image.resize((800, 400))
             new_image.save(filename)
         elif os.path.splitext(filename)[1]==".mp4":
-            size = subprocess.check_output(["ffprobe -v error -select_streams v:0 -show_entries\
-             stream=width,height -of csv=p=0 %s" % filename], shell=True).decode().strip().split(",")
+            size = subprocess.check_output("ffprobe" + " -v error -select_streams v:0 -show_entries\
+             stream=width,height -of csv=p=0 %s" % filename, shell=True).decode().strip().split(",")
             if size!=["800","400"]:
                 self.delete()
                 raise ValidationError("The video is not 800x400 in size. Upload some other video.")
